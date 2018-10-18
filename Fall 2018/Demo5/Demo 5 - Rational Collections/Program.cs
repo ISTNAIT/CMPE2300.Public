f5951633040104a5e6ada0d1743f52bfc6ca05cc
AJ Armstrong <aja@clanarmstrong.com>
2018-10-18 14:38:20 -0600

    Fun with lists!

---
+++ Fall 2018/Demo5/Demo 5 - Rational Collections/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_5___Rational_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            List<Rational> rats = new List<Rational>();
+            for(int i = 0; i < 10; ++i)
+                rats.Add(new Rational(r.Next(10) + 1, r.Next(10)+1));
+            if(r.Next(2) != 0) rats.Add(new Rational(7, 11));
+            PrintRats("Random",rats);
+            rats.Sort();
+            PrintRats("Sorted", rats);
+            int index = rats.BinarySearch(new Rational(7, 11));
+            Console.Write($"{new Rational(7, 11)} ");
+            if (index >= 0)
+                Console.WriteLine($"found at {index}.");
+            else
+                Console.WriteLine($"Not found.");
+            Console.Write($"The value is");
+            Console.Write(rats.Contains(new Rational(7, 11)) ? " " : " not ");
+            Console.WriteLine("found.");
+            rats.Remove(new Rational(7, 11));
+            PrintRats("Removed", rats);
+            index = rats.BinarySearch(new Rational(7, 11));
+            Console.Write($"{new Rational(7, 11)} ");
+            if (index >= 0)
+                Console.WriteLine($"found at {index}.");
+            else
+                Console.WriteLine($"Not found.");
+            rats.Reverse();
+            PrintRats("Reversed",rats);
+            rats.Sort(CompareDenominators);
+            PrintRats("Sorted by Denominator", rats);
+            PrintRats("Minimal Form", rats.FindAll(Rational.MinimalFormP));
+            PrintRats("Integral", rats.FindAll((q) => q.Denominator == 1));
+            rats.Clear();
+            PrintRats("Clear", rats);
+            Console.ReadLine();
+        }
+
+        static void PrintRats(string s, List<Rational> l)
+        {
+            Console.WriteLine(s);
+            foreach (char c in s)
+                Console.Write('-');
+            Console.WriteLine();
+            for(int i = 0; i < l.Count; ++i) //Count!
+                Console.Write(l[i] + " "); //Array!
+            Console.WriteLine();
+        }
+
+        public static int CompareDenominators(Rational left, Rational right)
+        {
+            return left.Denominator.CompareTo(right.Denominator);
+        }
+    }
+}
---
+++ Fall 2018/Demo5/Demo 5 - Rational Collections/Rational.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_5___Rational_Collections
+{
+    class Rational : IComparable
+    {
+        private int denominator = 1;
+
+        public Rational(int numerator, int denominator)
+        {
+            if (denominator == 0)
+                throw new DivideByZeroException("Denominator cannot be 0.");
+            Numerator = numerator;
+            this.denominator = denominator;
+        }
+
+        public Rational(int numerator) : this(numerator, 1) { }
+
+        public int Numerator { get; set; }
+        public int Denominator
+        {
+            get { return denominator; }
+            set
+            {
+                if (value == 0)
+                    throw new DivideByZeroException("Denominator cannot be 0.");
+                denominator = value;
+            }
+        }
+
+        private double RealValue
+        {
+            get { return (double)Numerator / Denominator; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Rational that)) return false;
+            //FIXME: should 1/2 == 2/4?
+            return (this.Numerator.Equals(that.Numerator)
+                && this.Denominator.Equals(that.Denominator));
+        }
+
+        public override int GetHashCode()
+        {
+            return 1;
+        }
+
+        public override string ToString() => $"{Numerator}/{Denominator}";
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Rational that))
+                throw new ArgumentException("Comparison to non-rational.");
+            return this.RealValue.CompareTo(that.RealValue);
+        }
+
+        //Predicate for minimal form
+        //Fixme: Brute force and ugly
+        public static bool MinimalFormP(Rational r)
+        {
+            //Choose larges of num, den
+            int limit = r.Numerator > r.Denominator ? r.Numerator : r.Denominator;
+            //Iterate over available possible common factors
+            for(int i = 2; i <= limit; ++i)
+            {
+                if (r.Numerator % i + r.denominator % i == 0) // Both divisable
+                    return false;
+            }
+            return true;
+        }
+
+    }
+
+    
+}
