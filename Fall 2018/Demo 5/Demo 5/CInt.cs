e877cf19a47188193324dc8fd7c174f32aa77a49
AJ Armstrong <aja@clanarmstrong.com>
2018-10-24 10:56:07 -0600

    Demo on set operations and delegates with List

---
+++ Fall 2018/Demo 5/Demo 5/CInt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo_5
+{
+    class CInt : IComparable
+    {
+        public int Value { get; set; }
+
+        public override string ToString() => Value.ToString();
+
+        public CInt(int value) { Value = value; }
+        public CInt() : this(0) { } 
+        public override bool Equals(object obj)
+        {
+            if (!(obj is CInt that)) return false;
+            return this.Value.Equals(that.Value);
+        }
+        public override int GetHashCode() => 1;
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is CInt that))
+                throw new ArgumentException("Not a CInt");
+            return this.Value.CompareTo(that.Value);
+        }
+    }
+}
