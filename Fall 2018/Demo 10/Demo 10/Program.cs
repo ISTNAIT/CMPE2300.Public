de05b4408fa82a86971c2e055dcc4bfe91c2ea8b
AJ Armstrong <aja@clanarmstrong.com>
2018-11-08 14:10:25 -0700

    First Inheritance Demo

---
+++ Fall 2018/Demo 10/Demo 10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Demo_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.WriteLine("==========================");
+            Trace.WriteLine("---Creating Vehicle---");
+            Vehicle v = new Vehicle();
+            Trace.WriteLine("---Creating Car---");
+            Car c = new Car();
+            c.Accelerate(100);
+            Trace.WriteLine($"Velocity = {c.Velocity}");
+            c.Accelerate(100);
+            Trace.WriteLine($"Velocity = {c.Velocity}");
+            c.Accelerate(100);
+            Trace.WriteLine($"Velocity = {c.Velocity}");
+            Trace.WriteLine($"The car named '{c}' was created");
+            Trace.WriteLine("==========================");
+        }
+    }
+}
